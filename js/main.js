ec7fb08cb46f4525947ffb6705a056c6006bdefd
alican1809 <94050902+alican1809@users.noreply.github.com>
2024-01-04 00:49:59 +0100

    upgrad

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const playBtn =document.getElementById("playBtn");
+
+playBtn.addEventListener("click",function () {
+    const home = document.getElementById('home');
+    home.style.display = 'none';
+    console.log(playBtn)
+})
+
+
+
+console.log(playBtn)
