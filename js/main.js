732136c2f86a435448dc78653d9a75463143c905
alican1809 <94050902+alican1809@users.noreply.github.com>
2024-01-30 00:41:02 +0100

    v2

--- js/main.js
+++ js/main.js
@@ -1,114 +1,159 @@
-let animationEnCours = false;
-const playBtn = document.getElementById("playBtn");
-const home = document.getElementById('home');
-const gameInterface = document.getElementById('gameInterface');
-const character = document.getElementById("character");
+let gameSection = document.getElementById("gameSection");
+let btnInterface = document.getElementById("btnInterface");
+let btnStart = document.getElementById("btnStart");
+let gameInterface = document.getElementById("gameInterface");
+let charactere = document.getElementById("charactere");
+let perso = document.getElementById("perso");
 
-let gameStarted = false;
-let bigObstacleCounter = 0;
-let obstacleInterval = 3000;
-let accelerationInterval = 10000;
-let obstacleCreationInterval;
+/////////////////////////////////outils
 
-playBtn.addEventListener("click", function() {
-  startGame();
-});
+function getRandomNbr(min, max) {
+    return Math.floor(Math.random() * (max - min) + min)}
 
-function startGame() {
-  gameStarted = true;
-  gameInterface.style.display = "none";
-  obstacleCreationInterval = setInterval(createObstacle, obstacleInterval);
-  setInterval(accelerateGame, accelerationInterval);
+/////////////////////////////////start
+
+async function start() {
+    console.log("btnStart a eté cliqué");
+    /* il faut enlever le bouton et activer la possibiliter des action en jeux*/    
+    btnInterface.classList.toggle("hidden");
+    console.log("btnInterface a hidden");
+    
 }
 
-function createObstacle() {
-  if (gameStarted) {
-    const bigObstacle = document.createElement('div');
-    bigObstacle.className = "obstacleBig";
-    const numberOfObstacles = Math.floor(Math.random() * 2) + 1;
-    const obstacleIndex = bigObstacleCounter += 1;
-    bigObstacle.setAttribute("obstacleIndex", `obstacle${obstacleIndex}`);
-    const numberOfImage= Math.floor(Math.random() * 2);
-
-    for (let i = 0; i < numberOfObstacles; i++) {
-      const smallObstacle = document.createElement("img");
-      smallObstacle.className = "obstacle";
-      smallObstacle.setAttribute("src",`asset/totems/totem${numberOfImage}.jpg`)
-      bigObstacle.appendChild(smallObstacle);
-    }
-
-    document.getElementById('map').appendChild(bigObstacle);
-
-    obstacleInterval = Math.floor(Math.random() * (4000 - 1000 + 1)) + 1000;
-
-    clearInterval(obstacleCreationInterval);
-    obstacleCreationInterval = setInterval(createObstacle, obstacleInterval);
-  }
-}
-
-function accelerateGame() {
-  if (gameStarted) {
-    obstacleInterval -= 500;
-    clearInterval(obstacleCreationInterval);
-    obstacleCreationInterval = setInterval(createObstacle, obstacleInterval);
-  }
-}
-
-function compareAndActOnObstacles() {
-  if (gameStarted) {
-  
-    let obstacles = document.querySelectorAll(".obstacleBig");
-    obstacles.forEach(function (obstacle) {
-      const style = window.getComputedStyle(obstacle);
-      const leftValue = parseFloat(style.getPropertyValue("left"));
-
-      const characterRect = character.getBoundingClientRect();
-      const obstacleRect = obstacle.getBoundingClientRect();
-
-      if (
-        characterRect.right > obstacleRect.left &&
-        characterRect.left < obstacleRect.right &&
-        characterRect.bottom > obstacleRect.top &&
-        characterRect.top < obstacleRect.bottom
-      ) {
-        gameInterface.style.display = "block";
-        gameStarted = false;
-        clearInterval(obstacleCreationInterval);
-        obstacle.remove();
-      }
-
-      if (leftValue + obstacle.offsetWidth < 0 ) {
-        obstacle.remove();
-        console.log("Obstacle supprimé car il est sorti de la page à gauche.");
-      }
-    });
-  }
-}
-
-
-setInterval(compareAndActOnObstacles, 10);
-
-document.addEventListener("click", function () {
-  if (gameStarted && !animationEnCours) {
-    jump();
-  }
-});
+/////////////////////////////////jump
+let jumpPlay = false;
 
 function jump() {
-  document.body.style.pointerEvents = "none";
-
-  character.classList.add("animJump");
-  animationEnCours = true;
-
-  setTimeout(function () {
-    document.body.style.pointerEvents = "auto";
-    removeJump();
-  }, 1000);
+    console.log("gameInterface a eté cliqué");
+    /*il faut que quand je clique sur game interface character saut*/
+    
+    charactere.classList.add("jump");
+    jumpPlay=true;
+    console.log("charactere a jump", charactere);
+    
+    /*ne pas pouvoi cliqué*/
+    gameInterface.style.pointerEvents = "none";
+    
+    console.log("gameInterface a ne peu pas clicker", charactere);
+    
+    /*delay remettre clicable et sur primmer le jup*/
+    setTimeout(function () {
+        charactere.classList.remove("jump")
+        gameInterface.removeAttribute("style")
+        jumpPlay = false;
+    }, 1000/*valeur egale de lanimation jump*/);
+    ;
+    
+    console.log("charactere a remov jump et mettre clicable gameInterface", charactere , gameInterface);
 }
 
-function removeJump() {
-  character.classList.remove("animJump");
-  animationEnCours = false;
+
+        gameInterface.addEventListener("click",function () {
+            if (!jumpPlay) {
+              jump();
+            }
+          });
+
+
+/////////////////////////////////creatObstacle
+
+let nbrTotem;
+let nbrTotemImg;
+
+
+let dataNbr = 0; 
+
+function addObstacle() {
+    const totemContainer = document.createElement("div");
+    
+    let nbra = getRandomNbr(1, 3)/* nbr de combien totem dans une tour*/
+    nbrTotem = nbra
+
+    let nbrb = getRandomNbr(0, 3)/* nbr definir image du totem changer i nom dimage change*/
+    nbrTotemImg =nbrb
+    ++dataNbr
+    if (nbrTotem == 1) {
+        totemContainer.id = `smallTotem${dataNbr}` 
+        totemContainer.setAttribute("data",`smallTotem${dataNbr}`)  
+        totemContainer.classList.add("smallTotem");
+    }
+    if(nbrTotem == 2){
+        totemContainer.id = `bigTotem${dataNbr}`
+        totemContainer.setAttribute("data",`bigTotem${dataNbr}`) 
+         totemContainer.classList.add("bigTotem");
+        }
+        totemContainer.classList.add("totemContainers");
+
+    for (let index = 0; index < nbrTotem; index++) {
+        const totem = document.createElement("img");
+        totem.setAttribute("src",`asset/totems/totem${nbrTotemImg}.jpg`);
+        totem.classList.add("totem");
+        totemContainer.appendChild(totem);
+    }
+    
+    gameInterface.appendChild(totemContainer);
+
+    translateObstacle(totemContainer);
 }
 
+
+
+
+function translateObstacle(totemContainer) {
+    totemContainer.classList.add("translate");
+    
+}
+
+
+
+
+
+
+let obstacleCreationInterval;  // Ajoutez cette variable globale
+
+btnStart.addEventListener("click", function () {
+
+        addObstacle();
+
+    obstacleCreationInterval = setInterval(removTotem, 10);
+    start();
+});
+
+function removTotem() {
+    let totemContainers = document.querySelectorAll(".totemContainers");
+    let lastTotemContainer = totemContainers[totemContainers.length - 1];  // Récupère le dernier totem créé
+
+    if (lastTotemContainer) {
+        const lastTotemRect = lastTotemContainer.getBoundingClientRect();
+
+        // Vérifie si le côté droit du dernier totem est inférieur à 800
+        if (lastTotemRect.right < 800) {
+            addObstacle();
+        }
+    }
+
+    totemContainers.forEach(function (totemContainer) {
+        const style = window.getComputedStyle(totemContainer);
+        const leftValue = parseFloat(style.getPropertyValue("left"));
+  
+        const charactereRect = charactere.getBoundingClientRect();
+        const totemContainerRect = totemContainer.getBoundingClientRect();
+       const ar=false
+        if (
+           charactereRect.right > totemContainerRect.left &&
+           charactereRect.left < totemContainerRect.right &&
+           charactereRect.bottom > totemContainerRect.top &&
+           charactereRect.top < totemContainerRect.bottom
+        ) {
+            btnInterface.classList.remove("hidden")
+            totemContainer.remove();
+        }
+
+        if (totemContainerRect.left + totemContainer.offsetWidth < 150) {
+            totemContainer.remove();
+            console.log("Obstacle supprimé car il est sorti de la page à gauche.");
+        }
+     
+    });
+}
 
---
+++ main.js
@@ -0,0 +1,114 @@
+let animationEnCours = false;
+const playBtn = document.getElementById("playBtn");
+const home = document.getElementById('home');
+const gameInterface = document.getElementById('gameInterface');
+const character = document.getElementById("character");
+
+let gameStarted = false;
+let bigObstacleCounter = 0;
+let obstacleInterval = 3000;
+let accelerationInterval = 10000;
+let obstacleCreationInterval;
+
+playBtn.addEventListener("click", function() {
+  startGame();
+});
+
+function startGame() {
+  gameStarted = true;
+  gameInterface.style.display = "none";
+  obstacleCreationInterval = setInterval(createObstacle, obstacleInterval);
+  setInterval(accelerateGame, accelerationInterval);
+}
+
+function createObstacle() {
+  if (gameStarted) {
+    const bigObstacle = document.createElement('div');
+    bigObstacle.className = "obstacleBig";
+    const numberOfObstacles = Math.floor(Math.random() * 2) + 1;
+    const obstacleIndex = bigObstacleCounter += 1;
+    bigObstacle.setAttribute("obstacleIndex", `obstacle${obstacleIndex}`);
+    const numberOfImage= Math.floor(Math.random() * 2);
+
+    for (let i = 0; i < numberOfObstacles; i++) {
+      const smallObstacle = document.createElement("img");
+      smallObstacle.className = "obstacle";
+      smallObstacle.setAttribute("src",`asset/totems/totem${numberOfImage}.jpg`)
+      bigObstacle.appendChild(smallObstacle);
+    }
+
+    document.getElementById('map').appendChild(bigObstacle);
+
+    obstacleInterval = Math.floor(Math.random() * (4000 - 1000 + 1)) + 1000;
+
+    clearInterval(obstacleCreationInterval);
+    obstacleCreationInterval = setInterval(createObstacle, obstacleInterval);
+  }
+}
+
+function accelerateGame() {
+  if (gameStarted) {
+    obstacleInterval -= 500;
+    clearInterval(obstacleCreationInterval);
+    obstacleCreationInterval = setInterval(createObstacle, obstacleInterval);
+  }
+}
+
+function compareAndActOnObstacles() {
+  if (gameStarted) {
+  
+    let obstacles = document.querySelectorAll(".obstacleBig");
+    obstacles.forEach(function (obstacle) {
+      const style = window.getComputedStyle(obstacle);
+      const leftValue = parseFloat(style.getPropertyValue("left"));
+
+      const characterRect = character.getBoundingClientRect();
+      const obstacleRect = obstacle.getBoundingClientRect();
+
+      if (
+        characterRect.right > obstacleRect.left &&
+        characterRect.left < obstacleRect.right &&
+        characterRect.bottom > obstacleRect.top &&
+        characterRect.top < obstacleRect.bottom
+      ) {
+        gameInterface.style.display = "block";
+        gameStarted = false;
+        clearInterval(obstacleCreationInterval);
+        obstacle.remove();
+      }
+
+      if (leftValue + obstacle.offsetWidth < 0 ) {
+        obstacle.remove();
+        console.log("Obstacle supprimé car il est sorti de la page à gauche.");
+      }
+    });
+  }
+}
+
+
+setInterval(compareAndActOnObstacles, 10);
+
+document.addEventListener("click", function () {
+  if (gameStarted && !animationEnCours) {
+    jump();
+  }
+});
+
+function jump() {
+  document.body.style.pointerEvents = "none";
+
+  character.classList.add("animJump");
+  animationEnCours = true;
+
+  setTimeout(function () {
+    document.body.style.pointerEvents = "auto";
+    removeJump();
+  }, 1000);
+}
+
+function removeJump() {
+  character.classList.remove("animJump");
+  animationEnCours = false;
+}
+
+
